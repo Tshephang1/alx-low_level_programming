0482f3a2f58f85bb67cb79d8ef7d594b1a301ce1
tshephang1 <tshephanghelter22@gmail.com>
2023-02-21 22:09:05 -0800

    Alpha in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet
+ * Discription: print alpha in lowercase
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	putchar(la);
+	putchar('\n');
+	return (0);
+}
