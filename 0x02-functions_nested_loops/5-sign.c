e18b68242cfcee9ce09dd0530334b58988acab0f
tshephang1 <tshephanghelter22@gmail.com>
2023-02-22 18:33:33 -0800

    hot fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_sign -> print sign based on condition
  * @n: argurment passed
- * Return: retun 1, 0, -1
+ * Return: retun 1, 0 -1
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 }
