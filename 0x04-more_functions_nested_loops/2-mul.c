250f981ac5cfbc57e8362fecdf4596c7f6846fa8
tshephang1 <tshephanghelter22@gmail.com>
2023-02-23 20:00:56 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul -miltyply 2 int
+ * @a: first int
+ * @b: second int
+ * Return:Additional of the multiplication
+ */
+
+int mal(int a, int b)
+{
+	return (a * b);
+}
