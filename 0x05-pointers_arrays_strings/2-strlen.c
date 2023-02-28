ac1635b026c2f1933d42701b4047fda7c5696a7a
tshephang1 <tshephanghelter22@gmail.com>
2023-02-27 16:46:38 -0800

    strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - find legnth of a string
+ * @s: pointer to the string to chech
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+
+	i++;
+
+	return (i);
+}
